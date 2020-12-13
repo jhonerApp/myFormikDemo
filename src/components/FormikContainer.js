bc0c2cc0323413cfc7a97340b53f779b145608f6
jhonerApp <jhonerApp@gmail.com>
2020-12-14 04:35:56 +0800

    Select tutorial 35

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -4,13 +4,22 @@ import * as Yup from "yup";
 import FormikControl from "./FormikControl";
 
 function FormikContainer() {
+  const dropdownOptions = [
+    { key: 'Select an option', value: '' },
+    { key: 'Option 1', value: 'Option 1' },
+    { key: 'Option 2', value: 'Option 2' },
+    { key: 'Option 3', value: 'Option 3' }
+
+  ]
   const initialValues = {
     email: "",
-    description: ""
+    description: "",
+    selectOption: ''
   };
   const validationSchema = Yup.object({
     email: Yup.string().required("Required!"),
-    description: Yup.string().required("Required!")
+    description: Yup.string().required("Required!"),
+    selectOption: Yup.string().required("Required!")
   });
   const onSubmit = (values) => console.log("Form data", values);
   return (
@@ -25,6 +34,7 @@ function FormikContainer() {
             <Form>
               <FormikControl control='input' type='email' label='Email' name='email' />
               <FormikControl control='textarea' type='description' label='Description' name='description' />
+              <FormikControl control='select' label='Select a topic' name='selectOption' options={dropdownOptions} />
               <button type="submit">Submit</button>
             </Form>
           )}
