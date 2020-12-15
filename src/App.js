a62a247ea932d42e8e607e8cfc631fd94e2f736e
jhonerApp <jhonerApp@gmail.com>
2020-12-16 05:38:15 +0800

    Registration Form Tutorial 40

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import FormikContainer from "./components/FormikContainer";
 import LoginForm from "./components/LoginForm";
 import OldYoutubeForm from "./components/OldYoutubeForm";
+import RegistrationForm from "./components/RegistrationForm";
 import YoutubeForm from "./components/YoutubeForm";
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       {/* <YoutubeForm /> */}
       {/* <FormikContainer /> */}
-      <LoginForm />
+      {/* <LoginForm /> */}
+      <RegistrationForm />
     </div>
   );
 }
